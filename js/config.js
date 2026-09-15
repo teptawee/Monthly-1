acf2cbf87a354234fdc050a973bbe20287fc607d
teptawee <tep1986@gmail.com>
2026-09-15 12:25:27 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // ====== ตั้งค่าตรงนี้ ======
 window.APP_CONFIG = {
   // 🔗 ใส่ Web App URL จาก GAS
-  GAS_API_URL: 'https://script.google.com/macros/s/AKfycbxxWIUmGr3ETOBGXWxadOmDLv7AUV1TguBnU1Sg_64tb4v2aTghDPE3lwaixQGk4WdAlw/execc',
+  GAS_API_URL: 'https://script.google.com/macros/s/AKfycbxxWIUmGr3ETOBGXWxadOmDLv7AUV1TguBnU1Sg_64tb4v2aTghDPE3lwaixQGk4WdAlw/exec',
   
   // 🔑 ต้องตรงกับ API_KEY ใน Code.gs
   API_KEY: 'Teptawee-02'
